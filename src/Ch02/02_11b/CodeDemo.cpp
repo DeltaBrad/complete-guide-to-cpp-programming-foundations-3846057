bd1a70389394c341f28beb00e1e3def008fbad51
DeltaBrad <bdburkham@gmail.com>
2025-04-24 15:33:24 +0000

    using type casting

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -9,7 +9,8 @@ int main(){
     int fahrenheit = 100;
     int celsius;
 
-    celsius = fahrenheit;
+    celsius = (static_cast<float>(5) / 9.0) * (fahrenheit - 32); //5 has been converted from int to float, 9 has been converted from int to double, compiler will use largest type to perform math operation.
+    //celsius = (5 / 9) * (fahrenheit - 32); This would have a value of 0, since compiler would treat 5/9 as int/int, which would return the quotient of 0, then 0*X is 0.
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
@@ -19,8 +20,8 @@ int main(){
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    std::cout << "Integer part   : " << static_cast<int>(weight) << std::endl;
+    std::cout << "Fractional part: " << (int)((weight - int(weight))*10000) << std::endl; //this would show the decimal portion as a whole integer to 4 decimal places
 
     std::cout << std::endl << std::endl;
     return 0;
