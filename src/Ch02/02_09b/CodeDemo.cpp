0fa066540e71419c94dd14211814e10f5ab9f787
DeltaBrad <bdburkham@gmail.com>
2025-04-24 15:06:01 +0000

    how to use structures

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,18 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character{
+    std::string name; 
+    int level;
+    character_role role;
+};
+
+
 int main(){
-    
+    game_character Joe;
+    Joe.name = "Dirt";
+    Joe.level = 10;
+    Joe.role = character_role::sidekick; //Joe is now a structure that contains the data of name, level, and role. Structures allow you to assign a single variable several qualities.
     std::cout << std::endl << std::endl;
     return 0;
 }
