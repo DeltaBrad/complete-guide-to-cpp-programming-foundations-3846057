08adc5c8f57f5259b9bf732f9d36d6862206ade8
DeltaBrad <bdburkham@gmail.com>
2025-04-24 13:12:20 +0000

    integer values

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+int a, b = 5; //both a and b are int, b has been given initial value of 5, a is empty. These will also be global variables in this case
+
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    unsigned int positive;
+    positive = b - a; // this will be negative value, which causes rollover for unsigned int and would be 2^32 + value, or if using long int would be 2^64 + value
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
