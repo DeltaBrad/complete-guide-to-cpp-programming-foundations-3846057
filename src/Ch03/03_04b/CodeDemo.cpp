0ade6511c1eb010222260d00442fbce6dcbd7b3e
DeltaBrad <bdburkham@gmail.com>
2025-04-24 16:34:53 +0000

    postfix and prefix increments and decrements

--- src/Ch03/03_04b/CodeDemo.cpp
+++ src/Ch03/03_04b/CodeDemo.cpp
@@ -12,16 +12,16 @@ int main(){
     std::cout << "Initial Lives: " << player_lives << std::endl;
 
     // Postfix increment
-    std::cout << "Score using postfix increment: " << player_score << std::endl;
+    std::cout << "Score using postfix increment: " << player_score++ << std::endl; //++ adds 1 after current value used, player_score is now 11
 
     // Prefix increment
-    std::cout << "Score using prefix increment: " << player_score << std::endl;
+    std::cout << "Score using prefix increment: " << ++player_score << std::endl; //++ adds 1 before current value used
 
     // Postfix decrement
-    std::cout << "Lives using postfix decrement: " << player_lives << std::endl;
+    std::cout << "Lives using postfix decrement: " << player_lives-- << std::endl; //-- subtracts one after current value used, player_lives is now 2
 
     // Prefix decrement
-    std::cout << "Lives using prefix decrement: " << player_lives << std::endl;
+    std::cout << "Lives using prefix decrement: " << --player_lives << std::endl; //-- subtracts one before current value used
 
     std::cout << std::endl << std::endl;
     return 0;
