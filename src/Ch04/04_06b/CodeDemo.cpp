db70199c65aac2ab8e2bf1e7a98fe8ecd9481e10
DeltaBrad <bdburkham@gmail.com>
2025-04-24 20:34:29 +0000

    vector initializing

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -4,9 +4,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
-    
+    std::vector<std::string> checkpoints = {"Start", "Forest", "Castle"};
+    checkpoints.push_back("Cave");
+    checkpoints.push_back("Finish"); //push_back adds to the vector new elements to the end of vector
+    std::cout << checkpoints[3] << std::endl; //proof that cave got added to the end of vector
     std::cout << std::endl << std::endl;
     return 0;
 }
