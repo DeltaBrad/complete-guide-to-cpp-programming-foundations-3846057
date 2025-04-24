9883485f41e8ddcdb6f85ba0d3dc37b4771b054f
DeltaBrad <bdburkham@gmail.com>
2025-04-24 12:53:19 +0000

    using cin, cout, flush

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -1,12 +1,15 @@
 // Complete Guide to C++ Programming Foundations
 // Challenge 01_09
 // Terminal Interaction, by Eduardo Corpeño 
+/*Goal: ask user in terminal their name and display their name back to them*/
 
 #include <iostream>
-
+#include <string>
 int main(){
-    std::cout << "Hi There!" << std::endl;
-
+    std::string name; //defines variable "name" as string
+    std::cout << "Enter your name:" << std::flush; //does part of what endl does but stays on same line
+    std::cin >> name; //note: cin only works for a single word
+    std::cout << name << "? That name is... unique." << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
