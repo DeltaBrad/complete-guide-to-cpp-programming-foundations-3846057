30e9cda00231bbd1357b72e3145c5dade9417b90
DeltaBrad <bdburkham@gmail.com>
2025-04-24 16:58:21 +0000

    definitions and preprocessor directives

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -5,10 +5,16 @@
 #include <iostream>
 #include <cstdint>
 
+#define MAX_AMMO 500 //This defines the term MAX_AMMO to a value of 500. any instance of this term will have this value and is performed in the preprocessor
+//#define DEBUG
+
 int main(){
     int32_t ammo = 100;
     uint8_t health_items = 5;
 
+#ifdef DEBUG
+    std::cout << "[DEBUG] Starting game simulation..." << std:endl; //if DEBUG is defined, message will print
+#endif
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
 
