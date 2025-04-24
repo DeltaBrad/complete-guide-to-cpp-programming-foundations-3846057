4a60cd293426909cdc523bde01a292d462c7965b
DeltaBrad <bdburkham@gmail.com>
2025-04-24 16:45:40 +0000

    variable operations

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -12,15 +12,20 @@ int main(){
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Addition assignment
-
+    ammo += 10; //adds 10 to ammo
+    std::cout << "Ammo Update: " << ammo << std::endl;
     // Subtraction assignment
-
+    ammo -= 5; //subtracts 5 from ammo
+    std::cout << "Ammo Update: " << ammo << std::endl;
     // Multiplication assignment
-
+    incoming_zombies *= 2; //multiplies variable by 2 and stores value
+    std::cout << "Zombie Update: " << incoming_zombies << std::endl;
     // Division assignment
-
+    ammo /= 2; //divides variable by 2 and stores value
+    std::cout << "Ammo Update: " << ammo << std::endl;
     // Remainder assignment
-
+    ammo %= 6; //provides remainder after variable is divided by 6
+    std::cout << "Ammo Update: " << ammo << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
