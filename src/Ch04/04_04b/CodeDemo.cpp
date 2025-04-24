b81d645feb0b5dd3486f6b35fadf134886d16a87
DeltaBrad <bdburkham@gmail.com>
2025-04-24 19:46:32 +0000

    pointers and objects

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -7,24 +7,28 @@
 #include <chrono>
 
 // Type aliases for chrono classes
-using Clock = std::chrono::system_clock;
-using TimePoint = std::chrono::time_point<Clock>;
-using Duration = std::chrono::duration<double>;
+using Clock = std::chrono::system_clock; //sets Clock as alias for accessing system clock
+using TimePoint = std::chrono::time_point<Clock>; //sets TimePoint as alias for using time_point with system clock as input
+using Duration = std::chrono::duration<double>; //sets Duration as alias for using duration which stores time ticks
 
 int main(){
-    TimePoint start, end;
-    Duration elapsed_seconds;
+    TimePoint *start, *end; // makes start and end pointers
+    Duration *elapsed_seconds = new Duration(); //made elapsed_seconds a pointer with dynamic memory allocation
     std::string input;
 
-    start = Clock::now();
+    start = new TimePoint(Clock::now()); //initializes start to system clock value
 
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
     std::cin >> input;
 
-    end = Clock::now();
-    elapsed_seconds = end - start;
+    end = new Timepoint(Clock::now()); //sets end value to system clock
+    *elapsed_seconds = *end - *start; //sets elapsed_seconds pointer value to value of end pointer minus start pointer
 
-    std::cout << "Reaction time: " << elapsed_seconds.count() << "s" << std::endl;
+    std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl; //since elapsed_seconds is a pointer and not a member of Duration, replace the "." with "->" to use count since the member would precede pointer
+
+    delete start; //delete variables to free memory
+    delete end;
+    delete elapsed_seconds;
     
     std::cout << std::endl << std::endl;
     return 0;
