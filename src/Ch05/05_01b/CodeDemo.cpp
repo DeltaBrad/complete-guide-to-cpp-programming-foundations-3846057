c35bb3a2f0613a3e13bb5220e60d428edd5d2eed
DeltaBrad <bdburkham@gmail.com>
2025-04-25 16:08:57 +0000

    if statements

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -9,6 +9,24 @@ int main(){
     bool gamePaused = false;
     char action = 'x';
     
+    if (score > 1000)
+        std::cout << "Your score is over 1k." << std::endl;
+    if (score % 2 == 0)
+        std::cout << "Number is even." << std::endl;
+    else
+        std::cout << "Number is odd." << std::endl;
+
+    std::cout << "The action '" << action << "'is ";
+    if (action != 'a' && action != 'w' && action != 's' && action != 'd' &&
+        action != 'A' && action != 'W' && action != 'S' && action != 'D')
+        std::cout << "not ";
+    std::cout << "a movement key." << std::endl;
+
+    if (gamePaused)
+        std::cout << "The game is paused!" << std::endl;
+    else
+        std::cout << "The game is running!" << std::endl;
+    
     std::cout << std::endl << std::endl;
     return 0;
 }
