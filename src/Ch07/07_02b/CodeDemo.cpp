211ed2eca3268337f43eb6e9c7196da585bfdf0f
DeltaBrad <bdburkham@gmail.com>
2025-04-28 12:52:30 +0000

    define class

--- src/Ch07/07_02b/CodeDemo.cpp
+++ src/Ch07/07_02b/CodeDemo.cpp
@@ -6,6 +6,15 @@
 #include <vector>
 #include <string>
 
+class Inventory{                //class definition, only needs the name and semicolon to define class, but can open brackets to define what the class does
+private:
+    std::vector<std::string> *items; //points to a vector of items
+    int capacity;    // max items allowed
+};
+
+
+
+
 int main(){
     
     std::cout << std::endl << std::endl;
